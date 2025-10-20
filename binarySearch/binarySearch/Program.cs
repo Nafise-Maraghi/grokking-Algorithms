4de9aeab1e78e049e1e972d01e273c79e65b984a
Precious <nafiseh.maraghi@gmail.com>
2025-10-20 22:25:43 +0330

    add binary search

---
+++ binarySearch/binarySearch/Program.cs
@@ -0,0 +1,29 @@
+﻿static int binarySearch(int item, int[] numbers)
+{
+	int low = 0;
+	int high = numbers.Length - 1;
+
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (item < numbers[mid])
+		{
+			high = mid - 1;
+		}
+		else if (item > numbers[mid])
+		{
+			low = mid + 1;
+		}
+		else // the item is found
+		{
+			return mid;
+		}
+	}
+
+	// the item does not exist in the numbers
+	return -1;
+}
+
+
+Console.WriteLine(binarySearch(43, Enumerable.Range(0, 100).ToArray())); // 43
+Console.WriteLine(binarySearch(12, Enumerable.Range(1, 10).ToArray())); // -1
