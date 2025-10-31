bb67aa88ac2b1cd8d58d964d9b9fc98bd9dcba37
Precious <nafiseh.maraghi@gmail.com>
2025-10-31 16:20:48 +0330

    add new excercises

---
+++ exercises/chapter4/4_2/4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿static int RecursiveCount(int[] numbers)
+{
+	if (numbers.Length == 0)
+	{
+		return 0;
+	}
+	return 1 + RecursiveCount(numbers[1..]);
+}
+
+
+RecursiveCount(new int[] { 1, 3, 5, 2 }); // 4
---
+++ exercises/chapter4/4_3/4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿static int RecursiveMax(int[] numbers)
+{
+	if (numbers.Length == 1)
+	{
+		return numbers[0];
+	}
+
+	int nextMax = RecursiveMax(numbers[1..]);
+	
+	if (numbers[0] > nextMax)
+	{
+		return numbers[0];
+	}
+	return nextMax;
+}
+
+
+RecursiveMax(new int[] { 1, 3, 5, 2 }); // 5
