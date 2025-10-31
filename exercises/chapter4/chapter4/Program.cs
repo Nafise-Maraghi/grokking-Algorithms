2127067cc34196b348467799d7bad1f660315502
Precious <nafiseh.maraghi@gmail.com>
2025-10-31 15:42:52 +0330

    change repository structure

---
+++ exercises/chapter4/chapter4/Program.cs
@@ -0,0 +1,11 @@
+﻿static int RecursiveSum(int[] numbers)
+{
+	if (numbers.Length == 0)
+	{
+		return 0;
+	}
+	return numbers[0] + RecursiveSum(numbers[1..]);
+}
+
+
+RecursiveSum(new int[] { 1, 3, 5, 2}); // 11
